05639ebf200bbf584d9d1e160e1c127337c75b1f
dm1tro69 <dimolg22@gmail.com>
2022-06-29 19:25:11 +0300

    add css and img

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './styles.css'
+import Logo from './logo.png'
+import ReactLogo from './logo-react.svg'
 
 const App = () => {
     return (
         <div>
             <h1>React Typescript</h1>
+            <img src={Logo} alt="img"/>
+            <img src={ReactLogo} alt="img"/>
         </div>
     );
 };
