b6d83bccdd2829f28651db4d5d54cb1875c9f8cc
dm1tro69 <dimolg22@gmail.com>
2022-06-30 19:01:25 +0300

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from 'react'
 import './styles.css'
 import Logo from './logo.png'
 import ReactLogo from './logo-react.svg'
-import ClickCounter from "./ClickConter";
+import ClickCounter from './ClickConter'
 
 const App = () => {
-    return (
-        <div>
-            <h1> Hello!!  React Typescript - {process.env.NODE_ENV} {process.env.name}</h1>
-            <img src={Logo} alt="img"/>
-            <img src={ReactLogo} alt="img"/>
-            <ClickCounter/>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>
+        {' '}
+        Hello!! React Typescript - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={Logo} alt="img" />
+      <img src={ReactLogo} alt="img" />
+      <ClickCounter />
+    </div>
+  )
+}
 
-export default App;
+export default App
